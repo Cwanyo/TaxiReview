610bcb68215bed14b6a004f06ea7be8e74b38592
cwan <manwan_444@hotmail.com>
2017-10-19 00:42:24 +0700

    add camera

--- src/pages/find-taxi/find-taxi.ts
+++ src/pages/find-taxi/find-taxi.ts
@@ -7,6 +7,8 @@ import { TaxiDetailPage } from '../taxi-detail/taxi-detail';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 @Component({
   selector: 'page-find-taxi',
   templateUrl: 'find-taxi.html'
@@ -14,13 +16,41 @@ import { Observable } from 'rxjs/Observable';
 export class FindTaxiPage {
 
   taxis: Observable<any[]>;
+  public taxiPhoto: any;
+  public base64Image: string;
 
   constructor(
     public navCtrl: NavController,
-    db:AngularFireDatabase
+    private afDB:AngularFireDatabase,
+    private camera: Camera
   ) {
-    this.taxis = db.list('Taxis').valueChanges();
+
+    this.taxis = this.afDB.list('Taxis').valueChanges();
+    console.log("taxis",this.taxis);
+    
   }
+
+  takePhoto(){
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      saveToPhotoAlbum: true
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.taxiPhoto = this.base64Image;
+    }, (err) => {
+     // Handle error
+     console.log("Take a photo error",err);
+    });
+  }
+
   goToTaxiDetail(params){
     if (!params) params = {};
     this.navCtrl.push(TaxiDetailPage);
