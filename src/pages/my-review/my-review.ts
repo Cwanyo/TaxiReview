3abfe94ab8c9c20cf321acb0370fde4673d70bc5
cwan <manwan_444@hotmail.com>
2017-11-05 00:19:56 +0700

    add my-review func

--- src/pages/my-review/my-review.ts
+++ src/pages/my-review/my-review.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { TaxiDetailPage } from '../taxi-detail/taxi-detail';
-import { ProfilePage } from './../profile/profile';
 
 //Fire
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
-import { Subscription } from 'rxjs/Subscription';
+//import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -20,6 +19,7 @@ export class MyReviewPage {
 
   private user: firebase.User;
 
+  private myReviews: Observable<any>;
   private taxisImage = {};
   
   constructor(
@@ -38,37 +38,36 @@ export class MyReviewPage {
         return;
       }
       this.user = user;
-      //this.getListOfMyReviews();
+      this.getListOfMyReviews();
     });
   }
 
   getListOfMyReviews(){
     console.log("getListOfMyReviews");
-  }
-  /*getListOfMyReviews(){
     const myReviewsRef = this.afDB.list('Users/'+this.user.uid+'/Reviews/');
-    let myReviews = myReviewsRef.snapshotChanges();
-    let sub = myReviews.subscribe(myReviewsData=> {
-      //this.listOfTaxis = myReviewsData;
+    this.myReviews = myReviewsRef.snapshotChanges();
+    let sub = this.myReviews.subscribe(myreviewData => {
 
-      myReviewsData.forEach(r=>{
-        const taxiImages = this.afDB.list('Taxis/'+r.key+'/Images', ref => ref.limitToLast(1)).valueChanges();
-        let tiSub = taxiImages.subscribe(imagesData=>{
-          if(imagesData.length == 0){
+      myreviewData.forEach(r=>{
+        console.log(r.key);
+        const taxiImage = this.afDB.list('Taxis/'+r.key+'/Images', ref => ref.limitToLast(1)).valueChanges();
+        let imSub = taxiImage.subscribe(imageData => {
+          if(imageData.length == 0){
             console.log("Taxi image not exist");
-            this.taxisImage[r.key] = "assets/img/taxi.png";
+            //this.taxisImage[r.key] = "assets/img/taxi.png";
           }else{
             console.log("Taxi image exist");
-            this.taxisImage[r.key] = imagesData;
+            this.taxisImage[r.key] = imageData;
           }
-          tiSub.unsubscribe();
+
+          imSub.unsubscribe();
         });
       });
 
       sub.unsubscribe();
     });
-  }*/
-
+  }
+  
   goToTaxiDetail(params){
     if (!params) params = {};
     this.navCtrl.push(TaxiDetailPage,{taxiLicensePlate: params});
